3a93b03d04ccdc657fefc873ecc58be16cf89125
diwakar142008 <diwakar142008@gmail.com>
2026-02-14 10:14:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import uuid
 import requests
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
@@ -105,4 +106,6 @@ def check_status(app_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port=int(os.environ.get("PORT",8080))
+
+    app.run(host="0.0.0.0",port=port)
